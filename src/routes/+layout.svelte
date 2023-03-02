6109ccca9a89bb7dac4636141de3769a7e73109d
Oliver Oswald <oliver.oswald25@gmail.com>
2023-03-02 21:12:54 +0100

    added base design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+    import '../app.postcss';
     import { Navbar } from "$lib/components";
+	import type { PageData } from './$types';
+    export let data: PageData;
+
+    const { user } = data;
 </script>
 
-<Navbar />
-<slot />
+<div class="h-full from-primary to-secondary bg-gradient-to-br p-4">
+    <Navbar {user}/>
+    <slot />
+</div>
