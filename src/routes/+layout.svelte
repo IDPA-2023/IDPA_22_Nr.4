9121f3c737eac8984e17e8d04c048d33ef2b3f0c
Oliver Oswald <oliver.oswald25@gmail.com>
2023-03-03 12:33:36 +0100

    added toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
     import '../app.postcss';
     import { Navbar } from "$lib/components";
 	import type { PageData } from './$types';
+    import { Toaster } from 'svelte-french-toast';
     export let data: PageData;
 
     const { user } = data;
 </script>
 
+<Toaster />
 <div class="h-full from-primary to-secondary bg-gradient-to-br p-4">
     <Navbar {user}/>
     <div class="py-4">
