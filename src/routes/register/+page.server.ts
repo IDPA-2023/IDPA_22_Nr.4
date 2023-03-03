60e19635e1962cf281d2732670ad5bca15cda18c
Oliver Oswald <oliver.oswald25@gmail.com>
2023-03-03 12:28:36 +0100

    added form validation

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,19 +1,29 @@
-import { error, redirect } from '@sveltejs/kit';
-import { generateUsername } from '$lib/utils';
+import { registerUserSchema } from '$lib/schemas';
+import { generateUsername, validateData } from '$lib/utils';
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { ClientResponseError } from 'pocketbase';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
 	register: async ({ locals, request }) => {
-		const body = Object.fromEntries(await request.formData());
+		const { formData, errors } = await validateData(await request.formData(), registerUserSchema);
 
-		let username = generateUsername((body.name as string).split(' ').join('')).toLowerCase();
+		if (errors) {
+			return fail(400, {
+				data: formData,
+				errors: errors.fieldErrors
+			});
+		}
+
+		let username = generateUsername(formData.name.split(' ').join('')).toLowerCase();
 
 		try {
-			await locals.pb.collection('users').create({ username, ...body });
-			await locals.pb.collection('users').requestVerification(body.email as string);
+			await locals.pb.collection('users').create({ username, ...formData });
+			await locals.pb.collection('users').requestVerification(formData.email as string);
 		} catch (err) {
-			console.log('Error: ', err);
-			throw error(500, 'Something went wrong');
+			console.error(err);
+			const e = err as ClientResponseError;
+			throw error(e.status, e.message);
 		}
 
 		throw redirect(303, '/login');
