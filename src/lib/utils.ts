92ec42ef43607a55094ce545d76f8d50555c92da
Oliver Oswald <oliver.oswald25@gmail.com>
2023-03-03 11:15:46 +0100

    added generate username function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
-export const serializeNonPOJOs = <T>(obj: T): T => { 
-    return structuredClone(obj);
-}
+const { randomBytes } = await import('node:crypto');
+
+export const serializeNonPOJOs = <T>(obj: T): T => {
+	return structuredClone(obj);
+};
+
+export const generateUsername = (name: string) => {
+	const id = randomBytes(2).toString('hex');
+	return `${name.slice(0, 5)}${id}`;
+};
