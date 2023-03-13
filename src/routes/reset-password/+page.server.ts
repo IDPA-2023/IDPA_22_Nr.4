5cd5583b5dc1b94aaf6200620881ce9699ae8cd1
Leandro200409 <78601676+Leandro200409@users.noreply.github.com>
2023-03-13 22:08:06 +0100

    Password reset page added

---
+++ src/routes/reset-password/+page.server.ts
@@ -0,0 +1,20 @@
+
+import { error, type Actions } from "@sveltejs/kit";
+
+export const actions: Actions = {
+    resetPassword:async ({ request, locals }) => {
+        const body = Object.fromEntries(await request.formData())
+
+        try {
+            if(body.email){
+                await locals.pb.collection('users').requestPasswordReset(body.email)
+            }
+            return {
+                success: true
+            }
+        }catch (err){
+            console.log('Error: ', err);
+            throw error(500, 'Something went wrong')
+        }
+    }
+}
---
+++ src/routes/reset-password/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let form
+</script>
+
+<div class="flex flex-col items-center h-full w-full">
+    <h2 class="mt-2 text-center text-3xl font-bold tracking-tight text-base-content">
+        Password zurücksetzen
+    </h2>
+    <p class="text-center mt-1">
+        Wir werden Ihnen eine E-mail mit dem Link zum Zurücksetzen Ihres Passwortes senden.
+    </p>
+    <form action="?/resetPassword" method="POST" class="flex flex-col items-center space-y-2 w-full pt-4">
+        <div class="form-control w-full max-w-md">
+            <label for="email" class="label font-medium pb-1">
+                <span class="label-text">E-mail</span>
+            </label>
+            <input type="email" name="email" class="input input-borderred w-full max-w-md">
+        </div>
+        <div class="w-full max-w-md pt-2">
+            <button type="submit" class="btn btn-primary w-full">Passwort Zurücksetzung bestätigen</button>
+        </div>
+        {#if form?.success}
+        <div class="alert alert-success shadow-lg w-full max-w-md">
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              <span>Zurücksetzungs- E-mail erfolgreich gesendet!</span>
+            </div>
+        </div>
+        {/if}
+    </form>
+</div>
