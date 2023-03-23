134c4d48288669fc42ad82f2a9526c5220eb99d9
Yannickqb <yanni.ledermann@gmail.com>
2023-03-23 10:20:17 +0100

    update

---
+++ src/routes/fillIn/multiplechoice/+page.svelte
@@ -0,0 +1,17 @@
+
+<div class="flex flex-col items-center h-hull w-full">
+	<div class="card w-full max-w-fit bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title mt-2 text-center text-3xl font-bold tracking-tight text-base-content">
+				Wer soll präsident werden
+			</h2>
+			<form method="post" action="?/createvote">
+			<div class="flex flex-col">
+				
+			</div>
+			<button type="submit">weiter</button>
+			</form>
+				
+		</div>
+	</div>
+</div>
---
+++ src/routes/fillIn/yesorno/+page.svelte
@@ -0,0 +1,15 @@
+<div class="flex flex-col items-center h-hull w-full">
+	<div class="card w-full max-w-fit bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title mt-2 text-center text-3xl font-bold tracking-tight text-base-content">
+				Pipi pupud d d d d  d d 
+			</h2>
+			<div class="btn-group btn-group-vertical lg:btn-group-horizontal items-center">
+				<button class="btn btn-active">Yes</button>
+				<button class="btn">No</button>
+			</div>
+			
+				
+		</div>
+	</div>
+</div>
