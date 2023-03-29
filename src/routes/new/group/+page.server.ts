f7444477bbb25c74aa56119750c14aed1548b6e0
Leandro200409 <78601676+Leandro200409@users.noreply.github.com>
2023-03-29 20:45:37 +0200

    Added functionality to create a new group

--- src/routes/new/group/+page.server.ts
+++ src/routes/new/group/+page.server.ts
@@ -9,17 +9,45 @@ export const load : PageServerLoad = ({ locals }) => {
     if(!locals.pb.authStore.isValid){
         throw redirect(303, '/login');
     }
+    return{
+        users: users
+    }
 }
 
 export const actions : Actions = {
     getUserByUsername: async ({ request, locals }) => {
         const data = Object.fromEntries(await request.formData())
-        console.log(data)
+        console.log(data.username)
         try {
             const record = serializeNonPOJOs<User>(await locals.pb.collection('users').getFirstListItem(`username="${data.username}"`))
             users.push(record)
+            console.log(record)
         } catch (err) {
-            console.log('ERROR: '+err)
+            console.log(err)
         }
+    },
+    createGroup: async ({ request, locals }) => {
+      const data = Object.fromEntries(await request.formData())
+      
+      try {
+        const createGroupData = {
+            "hostIDFS": locals.user?.id,
+            "name": data.name
+          }
+    
+          const group = await locals.pb.collection('group').create(createGroupData)
+
+          console.log(users)
+          for(let i=0; i< users.length; i++){
+            const userGroupData = {
+                "userIDFS": users[i].id,
+                "groupIDFS": group.id,
+                "weight": 1
+            }
+            const usergroup = await locals.pb.collection('userGroup').create(userGroupData) 
+          }
+      } catch (err) {
+        console.log(err)
+      }
     }
 }
--- src/routes/new/group/+page.svelte
+++ src/routes/new/group/+page.svelte
@@ -4,18 +4,32 @@
 
     export let data : PageData
 
-    console.log(data)
 </script>
 
 <div class="w-full h-full mt-5">
     <h3 class="font-medium text-2xl w-fit ml-auto mr-auto">Gruppe erstellen</h3>
-    <form action="?/getUserByUsername" method="post">
-        <div class="w-fit mr-auto ml-auto flex">
-            <Input type="text" name="username" required={true} label="Benutzername des einzuladenen Benutzers" id="username"/>
-            <button type="submit" class="w-fit btn btn-primary ml-px mb-2 mt-auto">+</button>
+    <form action="?/createGroup" method="post">
+        <div class="w-1/3 mr-auto ml-auto">
+            <Input type="text" name="name" required={true} label="Gruppenname" id="name" placeholder="PollToll Gruppe"/>
+        </div>
+        <form action="?/getUserByUsername" method="post">
+            <div class="w-1/3 mr-auto ml-auto flex mt-8">
+                <Input type="text" name="username" required={true} label="Benutzername des einzuladenen Benutzers" id="username" placeholder="username1234"/>
+                <button type="submit" class="w-fit btn btn-secondary ml-px mb-2 mt-auto">+</button>
+            </div>
+        </form>
+    
+        <h3 class="font-medium text-2xl w-fit ml-auto mr-auto">Bereits hinzugefügte Benutzer</h3>
+        {#if data.users}
+            <ul class="list-disc w-fit mr-auto ml-auto mt-5">
+            {#each data.users as user}
+            <li>Benutzername: <strong>{user.username}</strong>, Name: {user.name}</li>
+            {/each}
+            </ul>
+        {/if}
+        <div class="w-full flex mt-5">
+            <button type="submit" class="btn btn-primary ml-auto mr-auto">Gruppe erstellen</button>
         </div>
     </form>
-
-    <h3 class="font-medium text-2xl w-fit ml-auto mr-auto">Bereits hinzugefügte Benutzer</h3>
 </div>
 
