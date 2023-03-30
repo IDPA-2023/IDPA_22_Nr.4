e4a45ec1459a18cab58243bf9808e8034d6f4e94
Oliver Oswald <oliver.oswald25@gmail.com>
2023-03-30 16:46:39 +0200

    fixed problem with reactivity

--- src/lib/components/vote/YesOrNo.svelte
+++ src/lib/components/vote/YesOrNo.svelte
@@ -5,6 +5,10 @@
 
 	let selected: string;
 	$: selected = "";
+
+	function select(e: MouseEvent) {
+		selected = selected === (e.target as HTMLInputElement)?.value ? "" : (e.target as HTMLInputElement)?.value;
+	}
 </script>
 
 <div class="card w-full mt-4 bg-base-100 shadow-xl">
@@ -13,8 +17,8 @@
 			{question.question}
 		</h2>
 		<div class="flex flex-col">
-			<input type="button" on:click={() => selected = "Nein"} value="Nein" class="btn btn-secondary w-full mt-2 {selected === "Nein" ? "" : "btn-outline"}" />
-			<input type="button" on:click={() => selected = "Ja"} value="Ja" class="btn btn-secondary w-full mt-2 {selected === "Ja" ? "" : "btn-outline"}" />
+			<input type="button" on:click={select} value="Nein" class="btn btn-secondary w-full mt-2 {selected === "Nein" ? "" : "btn-outline"}" />
+			<input type="button" on:click={select} value="Ja" class="btn btn-secondary w-full mt-2 {selected === "Ja" ? "" : "btn-outline"}" />
 		</div>
 	</div>
 </div>
