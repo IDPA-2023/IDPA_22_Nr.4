aceca713559cb883533db449adb045004b2afdd0
Oliver Oswald <oliver.oswald25@gmail.com>
2023-04-03 21:28:08 +0200

    subscribed to search input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { PollCard } from '$lib/components';
-	import { createSearchStore } from '$lib/stores/search';
+	import { createSearchStore, searchHandler } from '$lib/stores/search';
+	import { onDestroy } from 'svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 
@@ -10,12 +11,18 @@
     }))
 
     const searchStore = createSearchStore(searchPolls);
+
+    const unsubscribe = searchStore.subscribe((model) => searchHandler(model));
+
+    onDestroy(() => {
+        unsubscribe();
+    });
 </script>
 
 <div class="flex flex-col gap-3">
     <div class="flex flex-row w-full">
         <h1 class="text-xl font-bold">Aktuelle Abstimmungen</h1>
-        <input type="text" class="ml-auto input input-bordered" placeholder="Suchen..." />
+        <input type="text" class="ml-auto input input-bordered" placeholder="Suchen..." bind:value={$searchStore.search} />
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {#each $searchStore.filtered as poll}
